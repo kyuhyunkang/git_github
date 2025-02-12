4666a722fe420288983e2876bd2556385118f14e
kyuhyunkang <kangkang119@naver.com>
2025-02-12 13:20:47 +0900

    fix

--- basic_syntax/basic_command.sh
+++ basic_syntax/basic_command.sh
@@ -1,3 +1,10 @@
 
 # git add . 은 모든 변경(추가) 사항을 staging area 이동
 git add .
+
+# git commit은 Stage area의 변경사항을 확정짓고 commitID생성
+
+git commit -m "메시지"
+
+# 원격 repo에 main 브랜치 변경사항(commit ID)을 업로드
+git push origin main
